8f883d3ce97807e13a6dbe107698e43a3d83303d
freelancersifat627e <freelancersifat627@gmail.comm>
2025-05-12 12:19:57 +0600

    2nd probleam solved

--- backpack/index.js
+++ backpack/index.js
@@ -1,5 +1,4 @@
 //* 1.Task: Array Filtering and Mapping
-
 //! Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
 
 const users = [
@@ -47,4 +46,43 @@ function arrayFilteringAndMapping() {
   return result;
 }
 
-console.log(arrayFilteringAndMapping());
+// console.log(arrayFilteringAndMapping());
+
+//* 5.Task: Array Reduction
+//! Create an array of numbers. Write a function that uses the reduce method to calculate the sum of all even numbers in the array
+
+function sumOfAllEvenNum(numbers) {
+  //   if (numbers / 2 === 0) {
+  //     console.log("this is even number");
+  //   } else {
+  //     console.log("this is odd number");
+  //   }
+
+  //! Way 1
+
+  //   let sum = 0;
+
+  //   for (number of numbers) {
+  //     console.log("number", number);
+  //     if (number % 2 === 0) {
+  //       console.log("this is even number");
+
+  //       sum += number;
+  //       console.log(sum);
+  //     } else {
+  //       console.log("this is odd number");
+  //     }
+  //   }
+
+  //! Way 2
+
+  const filterEvenNum = numbers.filter((number) => number % 2 === 0);
+  //   console.log(filterEvenNum);
+  const sumOfAllEvenNum = filterEvenNum.reduce(
+    (acc, current) => acc + current,
+    0
+  );
+  return sumOfAllEvenNum;
+}
+const numbers = [20, 45, 62, 31, 96, 55, 12, 18, 34, 36, 51];
+// console.log(sumOfAllEvenNum(numbers));
